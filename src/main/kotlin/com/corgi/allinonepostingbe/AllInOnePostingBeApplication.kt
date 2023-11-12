8efec7c4618bc8b599982bb92a66c5612343c5ed
이해원 <leehae2213@gmail.com>
2023-11-12 13:14:56 +0900

    feat: first init of spring boot 3.1.5 and java 21

---
+++ src/main/kotlin/com/corgi/allinonepostingbe/AllInOnePostingBeApplication.kt
@@ -0,0 +1,11 @@
+package com.corgi.allinonepostingbe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllInOnePostingBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<AllInOnePostingBeApplication>(*args)
+}
